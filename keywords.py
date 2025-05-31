4c81052ff9bd452870b2cdf9683a7ce5f5e1c944
neha <nehamutake9@gmail.com>
2025-05-31 20:30:28 +0530

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,14 @@
+
+import keyword
+
+klist = keyword.kwlist
+
+#print all the keywords
+print("All Python Keywords : ")
+for kw in klist:
+    print(kw)
+
+#ANOTHER WAY TO PRINT
+
+#import keyword
+#print(keyword.kwlist)
